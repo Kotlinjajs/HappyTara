8ea687918bea24373524f6d8b2edc37a4eba7880
Kotlinjajs <125186428+Kotlinjajs@users.noreply.github.com>
2023-08-20 23:35:11 +0330

    Add files via upload

---
+++ music/js/music-list.js
@@ -0,0 +1,74 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Perfect",
+    artist: "Ed Sheeran",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Can't Help Falling in Love",
+    artist: "Elvis Presley",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Make You Feel My Love",
+    artist: "Adele",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "I Will Always Love You",
+    artist: "Whitney Houston",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Your Song",
+    artist: "Elton John",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Thinking Out Loud",
+    artist: "Ed Sheeran",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Amazed",
+    artist: "Lonestar",
+    img: "music-7",
+    src: "music-7"
+   },
+  {
+    name: "All of Me",
+    artist: "John Legend",
+    img: "music-8",
+    src: "music-8"
+   },
+  {
+    name: "The Power of Love",
+    artist: "Celine Dion",
+    img: "music-9",
+    src: "music-9"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
